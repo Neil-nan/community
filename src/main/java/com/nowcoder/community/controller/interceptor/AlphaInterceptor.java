61f2cee343749f074dc347a3d62a314a381a7462
Neil <2378428641@qq.com>
2022-07-04 19:39:20 +0800

    2022/7/ login information

---
+++ src/main/java/com/nowcoder/community/controller/interceptor/AlphaInterceptor.java
@@ -0,0 +1,35 @@
+package com.nowcoder.community.controller.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+@Slf4j
+public class AlphaInterceptor implements HandlerInterceptor {
+
+    //在controller之前执行
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.debug("preHandle:" + handler.toString());
+        return true;
+    }
+
+    //在controller之后执行
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.debug("postHandle:" + handler.toString());
+    }
+
+    //在templateEngine之后执行
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        log.debug("afterCompletion:" + handler.toString());
+    }
+}
